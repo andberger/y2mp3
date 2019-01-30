2602dabc128725e70144d89193a80297e0e98141
andberger <and.bergsson@gmail.com>
2019-01-30 22:06:25 +0000

    Add build_url function

--- y2mp3.py
+++ y2mp3.py
@@ -1,6 +1,14 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number",
-                    type=int)
-args = parser.parse_args()
-print(args.square**2)
+
+def build_url(video_id):
+    youtube_watch_url = 'https://www.youtube.com/watch?v='
+    return youtube_watch_url + video_id
+
+def download_youtube_video_as_mp3(video_id):
+    url = build_url(args.video_id)
+    print(url)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('video_id', help='the video id given in the youtube watch url with the "v" parameter')
+    args = parser.parse_args()
